e479a163234287c6303fe9141280640f9e81bc82
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-05 17:34:08 -0300

    Adds sort and mergeArrays

--- functions.js
+++ functions.js
@@ -84,3 +84,38 @@ decodeMorse = function(morseCode){
     }
     return decodedMsg;
   } 
+
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+sortDescending = function(array) {
+  let length = array.length;
+  for (let counter = 0; counter <= length; counter += 1) {
+    for (let firstOfPair = 0, secondOfPair = 1; secondOfPair < length; firstOfPair += 1, secondOfPair += 1) {
+      if (array[firstOfPair] < array[secondOfPair]) {
+        let secondPlaceholder = array[secondOfPair];
+        array[secondOfPair] = array[firstOfPair];
+        array[firstOfPair] = secondPlaceholder;
+      }
+    }
+  }
+  return array;
+}
+simpleSortDescending = function(array) {
+  return array.sort((a, b) => b - a);
+}
+// console.log(simpleSortDescending([10, 9, 4, 7, 6, 1, 10, 2, 8, 5, 10]))
+
+// Merge arbitrary number of arrays given as input
+function mergeArrays() {
+  let allArrays = [];
+  for (let argument of arguments) {
+    for (let item of argument) {
+      allArrays.push(item);
+    }
+  }
+  return allArrays
+}
+function simpleMergeArrays( ...arrays) {
+  return arrays.flat();
+}
+// console.log(simpleMergeArrays([true, true], [1, 2], ['a', 'b']));
